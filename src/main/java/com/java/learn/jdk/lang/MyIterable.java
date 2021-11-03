2527295bd1facff4fd358a0a5faf636e82e98495
chenfuyuan <chenfuyuan0713@163.com>
2021-11-03 18:55:04 +0800

    feat(Collection): Collection-迭代器Iterator
    编写迭代器。

---
+++ src/main/java/com/java/learn/jdk/lang/MyIterable.java
@@ -0,0 +1,46 @@
+package com.java.learn.jdk.lang;
+
+import com.java.learn.jdk.util.MyIterator;
+import com.java.learn.jdk.util.MyObjects;
+
+import java.util.function.Consumer;
+
+/**
+ * 可进行迭代。
+ * 返回一个迭代器用于迭代。
+ * @author chenfuyuan
+ * @date 2021/10/31 21:02
+ */
+public interface MyIterable<T> {
+
+    /**
+     * 返回一个迭代器。用于元素的迭代
+     * @return 迭代器对象
+     */
+    MyIterator<T> iterator();
+
+    /**
+     * 进行迭代,将action函数作用到每个元素中
+     * @param action 函数
+     * @throws NullPointerException 如果action为null
+     */
+    default void forEach(Consumer<T> action) {
+        MyObjects.requireNonNull(action);
+        /*
+        因为时自己写的Iterable,所以无法使用for-each
+        for (T t : this) {
+            action.accept(t);
+        }
+        */
+        //自己写的for-each
+        //iterator().forEachRemaining(action);
+        MyIterator<T> iterator = iterator();
+        while (iterator.hasNext()) {
+            action.accept(iterator().next());
+        }
+    }
+
+
+
+
+}
---
+++ src/main/java/com/java/learn/jdk/util/MyIterator.java
@@ -0,0 +1,47 @@
+package com.java.learn.jdk.util;
+
+import java.util.Objects;
+import java.util.function.Consumer;
+
+/**
+ *
+ * @author chenfuyuan
+ * @date 2021/11/2 20:28
+ */
+public interface MyIterator<T> {
+
+    /**
+     * 如果还有下一个元素则返回 {@code true}
+     * （换句话说, 如果{@link #next} 返回一个元素，而不是抛出异常。则返回{@code true}）
+     * @return 如果还有下一个元素则返回{@code true}
+     */
+    boolean hasNext();
+
+    /**
+     * 下一个元素
+     * @return 下一个元素
+     * @throws java.util.NoSuchElementException 如果这个迭代器没有下一个元素
+     */
+    T next();
+
+    /**
+     *
+     * @throws UnsupportedOperationException 如果在这个迭代器中，不支持remove操作
+     */
+    default void remove(){
+        throw new UnsupportedOperationException("remove");
+    }
+
+    /**
+     * 对剩余的元素进行迭代。会将函数action，作用于每个剩余的元素中。
+     * @param action 函数
+     * @throws NullPointerException 如果action为null
+     */
+    default void forEachRemaining(Consumer<? super T> action) {
+        MyObjects.requireNonNull(action);
+        while (hasNext()) {
+            action.accept(next());
+        }
+    }
+
+}
